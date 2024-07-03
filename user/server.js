f6767a2e7fd29c06b87618af246d9656233ec382
cuong199344 <cuonghaxer2003@gmail.com>
2024-07-03 22:13:22 +0700

    test all

--- user/server.js
+++ user/server.js
@@ -4,11 +4,12 @@ import dbConnection from "./dbConfig/dbConnection.js";
 
 // MONGODB CONNECTION
 dbConnection();
-// Test 3
+// Test 4
 
 const PORT = process.env.PORT || 8800;
 
 const server = app.listen(PORT, () => {
+  console.log("testing");
   console.log(`Dev Server running on port test: ${PORT}`);
 });
 
